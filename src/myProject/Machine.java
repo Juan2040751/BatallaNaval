1c753c3a8653f25f93d019c1ed9a060f104493d6
2042577 William Velasco <william.velasco@correounivalle.edu.co>
2022-03-20 01:14:17 -0500

    define winner ready, documentation

--- src/myProject/Machine.java
+++ src/myProject/Machine.java
@@ -4,13 +4,20 @@ import java.util.Random;
 
 public class Machine {
     private String[] flota;
-    private int estaFlota,disparoX,disparoY;
+    private int estaFlota;
 
+    /**
+     * constructor of Machine class
+     */
     public Machine() {
         flota = new String[]{"portaaviones","submarino","submarino","destructor","destructor","destructor","fragata","fragata","fragata","fragata"};
         estaFlota = 0;
     }
 
+    /**
+     * to get the name (ship type) of each ship in the fleet
+     * @return the type of boat
+     */
     public String getBarco(){
         String barco="";
         if(estaFlota < 10) {
@@ -20,27 +27,47 @@ public class Machine {
         return barco;
     }
 
+    /**
+     * randomly defines the orientation in which the ship will be located
+     * @return a random orientation
+     */
     public String getOrientacion(){
         Random random = new Random();
         String[] orientacionMaquina = {"horizontal","vertical"};
         return (orientacionMaquina[random.nextInt(0,2)]);
     }
 
+    /**
+     * defines a random number between 0 and 10 that represents the coordinate on the x-axis
+     * @return coordinate on the x-axis
+     */
     public int getCoordenadaX(){
         Random random = new Random();
         return random.nextInt(0,10);
     }
 
+    /**
+     * defines a random number between 0 and 10 that represents the coordinate on the y-axis
+     * @return coordinate on the y-axis
+     */
     public int getCoordenadaY(){
         Random random = new Random();
         return random.nextInt(0,10);
     }
-    public void prepararDisparo(String[][] matrix){
-    }
+
+    /**
+     * defines a random number between 0 and 10 that represents the coordinate of the shot on the x-axis
+     * @return coordinate of the shot on the x-axis
+     */
     public int getDisparoX(){
         Random random = new Random();
         return random.nextInt(0,10);
     }
+
+    /**
+     * defines a random number between 0 and 10 that represents the coordinate of the shot on the y-axis
+     * @return coordinate of the shot on the y-axis
+     */
     public int getDisparoY(){
         Random random = new Random();
         return random.nextInt(0,10);
